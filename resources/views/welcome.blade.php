c35aad3f7dd0533a01e4ff046b3d9ab23e873d33
JOUETAxel <axel.jouet@devinci.fr>
2016-12-03 17:17:12 +0100

    Modification du ficher welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {!! '<strong>Rendu-Laravel</strong>' !!}
                 </div>
 
                 <div class="links">
