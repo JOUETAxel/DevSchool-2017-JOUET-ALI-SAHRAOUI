e176155c7d4cb4556864a80f771d32daaefeb4b8
JOUETAxel <axel.jouet@devinci.fr>
2016-12-03 19:40:42 +0100

    Ajout du Post.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+
+        $posts = Post::orderBy('id', 'desc')->paginate(10);
+
     }
 
     /**
---
+++ app/Models/Post.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Axel
+ * Date: 03/12/2016
+ * Time: 19:38
+ */
+
+namespace App\Http\Controllers;
+
+
+class Post
+{
+
+}
