89d31958315aabbfec22700c78416d11cd7b7067
JOUETAxel <axel.jouet@devinci.fr>
2016-12-03 19:54:18 +0100

    Modification du post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,15 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Axel
- * Date: 03/12/2016
- * Time: 19:38
- */
 
-namespace App\Http\Controllers;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-
+    //
 }
