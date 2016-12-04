4bf92b21c13e658141c4d25033cf7e649bdbdabf
JOUETAxel <axel.jouet@devinci.fr>
2016-12-04 19:02:50 +0100

    J'ai fais la class admin et le controller j'ai ajouter des choses dans le controller mais je sais pas si c'est comme ca du coup faut regarder si sa marche

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $post = Post::orderBy('id', 'desc')->paginate(5);
+        $event = Event::orderBy('id', 'desc')->paginate(5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create', 'events.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+        return view('posts.show','events.show' , compact('post','event'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        $event = Event::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::findOfFail($id);
+        $post->delete();
+
+        $event = Event::FindOrFail($id);
+        $event->delete();
+
+        return redirect()
+            ->route('post.index', 'event.index')
+            ->
+    }
+}
