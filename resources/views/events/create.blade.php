65265984fe04e4da5fa09777f66100f2f7288426
JOUETAxel <axel.jouet@devinci.fr>
2016-12-04 15:28:30 +0100

    Correction

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -12,7 +12,7 @@
 
                         {!! Form::open(array(
                         'route' => 'event.store',
-                         'method' => 'EVENT')) !!}
+                         'method' => 'POST')) !!}
 
                         {!! Form::label('title', 'Titre') !!}
                         {!! Form::text('title', null,
